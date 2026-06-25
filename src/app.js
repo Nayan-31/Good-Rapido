64c372710ada766a2cd6858bb7a5022d00adc4f1
Nayan Mahato <nayanmahato904@gmail.com>
2026-06-26 01:47:23 +0530

    refactor: restructure directories and fix import paths
    - Relocated middleware components to 'src/shared/middlewares/' for global reusability.
    - Relocated application constants to 'src/shared/constant/'.
    - Relocated 'db.js' to 'src/config/' to centralize configurations.
    - Updated path resolutions across 'server.js', 'app.js', and 'env.js' to correctly target the newly relocated shared assets.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import morgan from 'morgan'
 import env from './config/env.js'
-import securityMiddlewares from './middlewares/security.middleware.js';
+import securityMiddlewares from './shared/middlewares/security.middleware.js';
 
 export default function createApp() {
     const app = express()
--- src/shared/middlewares/rate-limiter.middleware.js
+++ src/shared/middlewares/rate-limiter.middleware.js
@@ -1,4 +1,4 @@
-import env from "../config/env.js"
+import env from "../../config/env.js"
 
 const request = new Map()
 const MAX_REQUEST = env.RATELIMIT
